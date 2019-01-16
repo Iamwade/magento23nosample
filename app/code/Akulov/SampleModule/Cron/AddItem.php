aa089c38eaaa3a91a73277474576429961fd8423
serhii <sergey.ak@interactivated.me>
2019-01-16 19:39:11 +0200

    1.11 System Configuration

--- app/code/Akulov/SampleModule/Cron/AddItem.php
+++ app/code/Akulov/SampleModule/Cron/AddItem.php
@@ -3,21 +3,27 @@
 namespace Akulov\SampleModule\Cron;
 
 use Akulov\SampleModule\Model\ItemFactory;
+use Akulov\SampleModule\Model\Config;
 
 class AddItem
 {
     private $itemFactory;
 
-    public function __construct(ItemFactory $itemFactory)
+    private $config;
+
+    public function __construct(ItemFactory $itemFactory, Config $config)
     {
         $this->itemFactory = $itemFactory;
+        $this->config = $config;
     }
 
     public function execute()
     {
-        $this->itemFactory->create()
-            ->setName('Last Scheduled item')
-            ->setDescription('Created at ' . time())
-            ->save();
+        if ($this->config->isEnabled()) {
+            $this->itemFactory->create()
+                ->setName('Scheduled item')
+                ->setDescription('Created at ' . time())
+                ->save();
+        }
     }
 }
